e6cf657ef34016f4ead0d99571ba516a1dd4e73b
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2022-01-27 10:14:44 -0500

    Add scripting for work section nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 "use strict";
+
+let workNavEntries = document.getElementsByClassName("workContents");
+let workNavIndex = 0;
+
+for (let i = 1; i < workNavEntries.length; i++) {
+    // Set every entry in the work section to nonvisible except for the first (most current) one.
+    workNavEntries[i].style.display = "none";
+}
+
+
+function workNavGoBack() {
+    if (workNavIndex > 0) {
+        workNavIndex--;
+        let curr = document.getElementById("workEntry" + workNavIndex);
+        let prev = document.getElementById("workEntry" + (workNavIndex + 1));
+        curr.style.display = "flex";
+        prev.style.display = "none";
+    }
+}
+
+function workNavGoForward() {
+    if (workNavIndex < workNavEntries.length - 1) {
+        workNavIndex++;
+        let curr = document.getElementById("workEntry" + workNavIndex);
+        let prev = document.getElementById("workEntry" + (workNavIndex - 1));
+        curr.style.display = "flex";
+        prev.style.display = "none";
+    }
+}
