aed8d19be3e0a5e62d947346acc419b3770ceed3
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2022-01-27 11:36:48 -0500

    Add nav button enabling/disabling

--- js/index.js
+++ js/index.js
@@ -8,10 +8,31 @@ for (let i = 1; i < workNavEntries.length; i++) {
     workNavEntries[i].style.display = "none";
 }
 
+function setNavButtonStates() {
+    let bPrev = document.getElementById("workNavPrev");
+    let bNext = document.getElementById("workNavNext");
+
+    if (workNavIndex <= 0) {
+        // Disable the prev button.
+        bPrev.firstElementChild.disabled = true;
+    }
+    else {
+        // Enable the prev button.
+        bPrev.firstElementChild.disabled = false;
+    }
+
+    if (workNavIndex >= workNavEntries.length - 1) {
+        bNext.firstElementChild.disabled = true;
+    }
+    else {
+        bNext.firstElementChild.disabled = false;
+    }
+}
 
 function workNavGoBack() {
     if (workNavIndex > 0) {
         workNavIndex--;
+        setNavButtonStates();
         let curr = document.getElementById("workEntry" + workNavIndex);
         let prev = document.getElementById("workEntry" + (workNavIndex + 1));
         curr.style.display = "flex";
@@ -22,6 +43,7 @@ function workNavGoBack() {
 function workNavGoForward() {
     if (workNavIndex < workNavEntries.length - 1) {
         workNavIndex++;
+        setNavButtonStates();
         let curr = document.getElementById("workEntry" + workNavIndex);
         let prev = document.getElementById("workEntry" + (workNavIndex - 1));
         curr.style.display = "flex";
